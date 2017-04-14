f05d9ff12127d56c3a1b30774f3fc1bf03aca21a
Aamir Ahmad <aamir.ahmad170@gmail.com>
2017-04-15 02:44:53 +0530

    initial commit

--- dockerfile-nginx.sh
+++ dockerfile-nginx.sh
@@ -4,8 +4,8 @@ ENV NGINX_VERSION 1.11.10~jessie
 RUN apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 573BFD6B3D8FBC641079A6ABABF5BD827BD9BF62 \
     &&  echo "deb http://nginx.org/packages/mainline/debian jessie nginx" >> /etc/apt/sources.list \ 
     &&  apt-get update \ --update apt cache burst
-    $$  apt-get install --no-install-recommends --no-install-suggests -y \ 
-                             ca-certificates -- packages to install
+    &&  apt-get install --no-install-recommends --no-install-suggests -y \ 
+                             ca-certificates 
                              nginx=${NGINX_VERSION} \ 
                              nginx-module-xslt \
                              nginx-module-geoip \
@@ -13,9 +13,11 @@ RUN apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 573BFD6B3D8FBC64107
                              nginx-module-perl \
                              nginx-module-njs \
                              getext-base \
-             && rm -rf /var/lib/apt/lists/*
+             
+                    && rm -rf /var/lib/apt/lists/*
+
 RUN ln -sf /dev/stdout /var/log/nginx/access.log \  
-RUN ln -sf /dev/stderr /var/log/nginx/error.log 
+ && ln -sf /dev/stderr /var/log/nginx/error.log 
 
 EXPOSE 80 443  
 CMD ["nginx","-g","daemon off;"] 

489f9eedb973f6f40d9bc2d10f1bcd60c5993fc4
Aamir Ahmad <aamir.ahmad170@gmail.com>
2017-04-15 03:18:29 +0530

    Changes and fixes

--- dockerfile-nginx.sh
+++ dockerfile-nginx.sh
@@ -3,9 +3,9 @@ MAINTAINER NGINX Docker Mainainers "nishantshrivastava1@gmail.com"
 ENV NGINX_VERSION 1.11.10~jessie  
 RUN apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 573BFD6B3D8FBC641079A6ABABF5BD827BD9BF62 \
     &&  echo "deb http://nginx.org/packages/mainline/debian jessie nginx" >> /etc/apt/sources.list \ 
-    &&  apt-get update \ --update apt cache burst
+    &&  apt-get update \ 
     &&  apt-get install --no-install-recommends --no-install-suggests -y \ 
-                             ca-certificates 
+                             ca-certificates \ 
                              nginx=${NGINX_VERSION} \ 
                              nginx-module-xslt \
                              nginx-module-geoip \
